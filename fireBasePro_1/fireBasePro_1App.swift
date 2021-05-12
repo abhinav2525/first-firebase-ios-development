7de54ab4d366c7f8943d2d370097a29b8d8255e3
abhinav2525 <56315478+abhinav2525@users.noreply.github.com>
2021-05-12 17:04:11 +0530

    Initial Commit

---
+++ fireBasePro_1/fireBasePro_1App.swift
@@ -0,0 +1,17 @@
+//
+//  fireBasePro_1App.swift
+//  fireBasePro_1
+//
+//  Created by Abhinav krishna on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct fireBasePro_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
